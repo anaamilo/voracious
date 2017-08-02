9750c6fe8253735c4c495e4a298a932d2308b080
Cduartez <Cduartez@github.com>
2017-08-02 18:15:00 +0200

    routes new food

--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import {HomepageComponent} from './homepage/homepage.component';
 import { FoodListComponent } from './food-list/food-list.component';
+import { NewFoodComponent } from './new-food/new-food.component';
 
 export const routes: Routes = [
     { path: '', component: HomepageComponent},
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'foods', component: FoodListComponent },
+    { path: 'foods/add', component: NewFoodComponent },
     { path: '**', redirectTo: '' }
 ];
