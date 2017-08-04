2d31ff030eb2028c8a8bd0072148c96e45d78a9b
salvocostahack <salvatorecostanzopa@gmail.com>
2017-08-04 12:39:02 +0200

    searchbar done

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -20,10 +20,9 @@ export class LoginComponent implements OnInit {
   login() {
     this.session.login(this.username,this.password)
       .subscribe(
-        (user) => console.log(user),
+        (user) =>   this.router.navigate(['/home']),
         (err) => this.error = err
       );
-      this.router.navigate(['/home']);
-      console.log(`${this.username} is logged`)
+      console.log(`${this.username} incorrect user`)
     }
   }
